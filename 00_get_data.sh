fceb7c0ab8d71cd5a3305e14c46678254be821d4
hscannell <scannell@ldeo.columbia.edu>
2021-08-26 11:08:14 -0400

    update-data

--- 00_get_data.sh
+++ 00_get_data.sh
@@ -1,10 +1,12 @@
 # Downloads gunzipped netCDF files via FTP from the UCSD server (kakapo). Files can also be downloaded directly from http://sio-argo.ucsd.edu/RG_Climatology.html 
 
-cd /glade/scratch/scanh/RG-argo-climatology
+# Change the path to where you want this data to be downloaded and uncomment the line below.
+# cd /Users/hillaryscannell/Documents/repos/mhw-2020/data
 
 wget ftp://kakapo.ucsd.edu/pub/gilson/argo_climatology/RG_ArgoClim_Salinity_2019.nc.gz
 wget ftp://kakapo.ucsd.edu/pub/gilson/argo_climatology/RG_ArgoClim_Temperature_2019.nc.gz
 wget ftp://kakapo.ucsd.edu/pub/gilson/argo_climatology/RG_ArgoClim_2019*
 wget ftp://kakapo.ucsd.edu/pub/gilson/argo_climatology/RG_ArgoClim_2020*
+wget ftp://kakapo.ucsd.edu/pub/gilson/argo_climatology/RG_ArgoClim_2021*
 
 gzip -d *
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mhw-2020
 This repo contains code to update figure 2 from Scannell et al. (2020) for the State of the Climate Report in 2020 Global Oceans Chapter.
+
+
+## Getting started
+
+1. clone this repository to your local or remote machine
+```bash
+git clone https://github.com/hscannell/mhw-2020.git
+```
+
+2. Set up the conda environment
+```bash
+conda env create -f environment.yml 
+conda activate mhw-2020
+```
+
